6d6bfc350629d9f287851385302033c60fb52c7f
IvanUsatov <andrappdevelop@gmail.com>
2023-05-16 21:29:16 +0700

    add service, register service

--- app/src/main/java/com/example/notificationapp/NotificationBroadcastReceiver.kt
+++ app/src/main/java/com/example/notificationapp/NotificationBroadcastReceiver.kt
@@ -7,8 +7,8 @@ import android.content.Intent
 class NotificationBroadcastReceiver : BroadcastReceiver() {
 
     override fun onReceive(p0: Context?, p1: Intent?) {
-        if (p1?.action.equals(Intent.ACTION_SCREEN_ON)) {
-            p0?.startService(Intent(p0, NotificatonService::class.java))
+        if (p1?.action.equals(Intent.ACTION_AIRPLANE_MODE_CHANGED)) {
+            p0?.startService(Intent(p0, NotificationService::class.java))
         }
     }
 }
---
+++ app/src/main/java/com/example/notificationapp/NotificationService.kt
@@ -0,0 +1,55 @@
+package com.example.notificationapp
+
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.Service
+import android.content.Intent
+import android.os.IBinder
+import androidx.core.app.NotificationCompat
+
+class NotificationService : Service() {
+
+    override fun onCreate() {
+        super.onCreate()
+    }
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+
+        showNotification()
+
+        return super.onStartCommand(intent, flags, startId)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+    }
+
+    override fun onBind(p0: Intent?): IBinder? {
+        return null
+    }
+
+    private fun showNotification() {
+
+        val notificationManager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+
+        val notificationChannel = NotificationChannel(
+            CHANNEL_ID,
+            CHANNEL_NAME,
+            NotificationManager.IMPORTANCE_DEFAULT
+        )
+        notificationManager.createNotificationChannel(notificationChannel)
+
+        val builder = NotificationCompat.Builder(this, CHANNEL_ID)
+            .setContentTitle("Title")
+            .setContentText("Text")
+            .build()
+
+        notificationManager.notify(1, builder)
+    }
+
+    companion object {
+
+        private const val CHANNEL_ID = "channel_id"
+        private const val CHANNEL_NAME = "channel_name"
+    }
+}
